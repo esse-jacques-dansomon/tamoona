bb36b1f3332feae1b0f64a9febe521afa1fe78a4
Esse Jacques DANSOMON <essedansomon@gmail.com>
2021-09-27 08:52:41 +0000

    Feature: Fin de compression des fichiers js et css

---
+++ public/js/custom-date.js
@@ -0,0 +1,1 @@
+$('#date-range0').dateRangePicker({autoClose:!0,singleDate:!0,showShortcuts:!1,singleMonth:!0,showTopbar:!1,extraClass:'reserved-form'});$('#date-range1').dateRangePicker({autoClose:!0,singleDate:!0,showShortcuts:!1,singleMonth:!0,showTopbar:!1,extraClass:'reserved-form'})
