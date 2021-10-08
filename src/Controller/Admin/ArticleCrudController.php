09bf1d47c7c4ff205b42f0eaa1c15cc6aaf7f823
Esse Jacques DANSOMON <essedansomon@gmail.com>
2021-10-08 17:44:09 +0000

    Feature design

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -6,7 +6,6 @@ use App\Entity\Article;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
@@ -63,7 +62,7 @@ class ArticleCrudController extends AbstractCrudController
                 ->setUploadDir("public/images/articles")
                 ->setRequired(false)
                 ->setUploadedFileNamePattern("[name][timestamp].[extension]"),
-            TextareaField::new('content', "Contenu de l'article")->setFormType(CKEditorType::class)->hideOnIndex(),
+            TextEditorField::new('content', "Contenu de l'article"),
 
         ];
     }
