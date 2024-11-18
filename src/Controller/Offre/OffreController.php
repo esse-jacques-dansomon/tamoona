a83c7527fdbaff2d6392a83193010f167afb6770
essejacques.me <essedansomon@gmail.com>
2024-11-18 11:24:52 +0000

    fix: my dmc

--- src/Controller/Offre/OffreController.php
+++ src/Controller/Offre/OffreController.php
@@ -35,7 +35,8 @@ class OffreController extends AbstractController
     public function index(OfferRepository $offerRepository, FeaturedOfferRepository $featuredOfferRepository)
     {
 
-        $offers = $offerRepository->findBy(['isDisplayed'=> true, 'category'=>'autres', 'id' => 'DESC']);
+        // $offers = $offerRepository->findBy(['isDisplayed'=> true, 'category'=>'autres', 'id' => 'DESC']);
+        $offers = $offerRepository->findBy(['isDisplayed'=>true ]);
         return $this->render('frontend/pages/offres.html.twig', [
             "offers"=>$offers,
             "featuredOffers"=>$featuredOfferRepository->findBy(['isActive'=> true],['id'=>'DESC'])
